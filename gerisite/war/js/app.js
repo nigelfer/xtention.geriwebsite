bafa73c7f0efde787f7d5e195f42a57fa62dcd8d
nigel <nigelfer.2@gmail.com>
2013-06-06 23:22:13 -0400

    added search, improved home page performance, moved kurtis to separate page

--- gerisite/war/js/app.js
+++ gerisite/war/js/app.js
@@ -1,18 +1,13 @@
-//Google search code
-(function() {
-	var cx = '002612420283441268524:sumneqdctam';
-	var gcse = document.createElement('script');
-	gcse.type = 'text/javascript';
-	gcse.async = true;
-	gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:')
-			+ '//www.google.com/cse/cse.js?cx=' + cx;
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(gcse, s);
-})();
+var searchShown = false;
 
 $(function() {
-
-	/* hiding and showing the Different menu sections */
+	
+	/* ######## lazy load images ######## */
+	$("img.lazy").lazyload({
+		container: $("#slideshowContainer")
+	});
+	
+	/* ######## hiding and showing the Different menu sections ######## */
 	$home = $('#home');
 	$about = $('#about');
 	$products = $('#products');
@@ -22,7 +17,7 @@ $(function() {
 	$tshirts = $('#products_tshirts');
 	$jewellery = $('#products_jewellery');
 	$contact = $('#contact');
-
+	$search = $('#searchSection');
 
 	var hideAll = function() {
 		$home.hide();
@@ -34,16 +29,18 @@ $(function() {
 		$tshirts.hide();
 		$jewellery.hide();
 		$contact.hide();
+		$search.hide();
 	};
 
 	hideAll();
 	$home.fadeIn(10);
-	
+
 	// Bind a handler for state
 	$.History.bind('', function(state) {
 		// Show apricots tab, hide the other tabs
 		hideAll();
 		$home.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 
 	// Bind a handler for state: apricots
@@ -51,81 +48,79 @@ $(function() {
 		// Show apricots tab, hide the other tabs
 		hideAll();
 		$about.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/products', function(state) {
 		hideAll();
 		$products.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/products/kurtis', function(state) {
 		hideAll();
-		$kurtis.stop(true, true).fadeIn(200);
-		
-		// Hook up the FlexSlider
-		var Main = Main || {};
-		
-		window.responsiveFlag = jQuery('#responsiveFlag').css('display');
-		Main.gallery = new Gallery();
-		
-		jQuery(window).resize(function() {
-			Main.gallery.update();
-		});
-		
-		function Gallery(){
-			var self = this,
-				container = jQuery('.flexslider'),
-				clone = container.clone( false );
-				
-			this.init = function (){
-				if( responsiveFlag == 'block' ){
-					var slides = container.find('.slides');
-					
-					slides.kwicks({
-						sticky: true,
-						max : 700,
-						spacing : 5
-					}).find('li > a').click(function (){
-						return false;
-					});
-				} else {
-					container.flexslider();
-				}
-			}
-			this.update = function () {
-				var currentState = jQuery('#responsiveFlag').css('display');
-				
-				if(responsiveFlag != currentState) {
-				
-					responsiveFlag = currentState;
-					container.replaceWith(clone);
-					container = clone;
-					clone = container.clone( false );
-					
-					this.init();	
-				}
-			}
-			
-			this.init();
-		}		
-		
+		// $kurtis.stop(true, true).fadeIn(10);
+		$kurtis.load('kurtis.html');
+		$kurtis.stop(true, true).fadeIn(10);
+		searchShown = false;
 	});
 	$.History.bind('/products/western', function(state) {
 		hideAll();
 		$western.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/products/tunics', function(state) {
 		hideAll();
 		$tunics.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/products/tshirts', function(state) {
 		hideAll();
 		$tshirts.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/products/jewellery', function(state) {
 		hideAll();
 		$jewellery.stop(true, true).fadeIn(200);
+		searchShown = false;
 	});
 	$.History.bind('/contact', function(state) {
 		hideAll();
 		$contact.stop(true, true).fadeIn(200);
+		searchShown = false;
+	});
+
+	/* ######## Search ####### */
+	$("#filter").keyup(function() {
+
+		// Retrieve the input field text and reset the count to zero
+		var filter = $(this).val(), count = 0;
+
+		if (filter.length == 0) {
+			return;
+		} else {
+			if (searchShown != true) {
+				hideAll();
+				$search.stop(true, true).fadeIn(200);
+				searchShown = true;
+			}
+		}
+
+		// Loop through the comment list
+		$(".searchdata li").each(function() {
+
+			// If the list item does not contain the text phrase fade it out
+			if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+				$(this).fadeOut();
+
+				// Show the list item if the phrase matches and increase the
+				// count by 1
+			} else {
+				$(this).show();
+				count++;
+			}
+		});
+
+		// Update the count
+		var numberItems = count;
+		$("#filter-count").text(count + " Search results");
 	});
 });
